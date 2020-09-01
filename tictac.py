2baffdf8498d6e603b0fb1cbeaf62ee1fa2bf673
beegeiger <beegeiger@gmail.com>
2020-09-01 15:15:50 -0700

    Working on updated algorithim

--- tictac.py
+++ tictac.py
@@ -185,11 +185,11 @@ def check_state(board):
     elif option.count("X") == board_size - 1 and option.count("O") == 0:
       for o in option:
         if o != "X":
-          return ["X endgame", o]
+          return ["X", o]
     elif option.count("O") == board_size - 1 and option.count("X") == 0:
       for p in option:
         if p != "O":
-          return ["O endgame", p]
+          return ["O", p]
   # print("Print 7.3")
   if board.count("X") + board.count("O") == (board_total):
     return "Tie"
@@ -198,6 +198,74 @@ def check_state(board):
 loop_num = 0
 
 def check_options(board):
+  # print("Print 8.1")
+  global A
+  global B
+  global board_size
+  global board_total
+  global loop_num
+  board_copy = []
+  board_copy_copy = []
+  chances = []
+  # print("Print 8.2")
+  for n in range(board_total):
+    # print("Print 8.3")
+    chances.append(0)
+  for y in range(board_total):
+    # print("Print 8.4")
+    loop_num += 1
+    board_copy = list(board)
+    # print("LOOP NUMBER: ", loop_num, y, board_copy[y], B, board_copy)
+    if str(board_copy[y]).isdigit():
+      # print("Print 8.5", board_copy)
+      board_copy[y] = B
+      # print("Print 8.55", board_copy, y, chances)
+      cur_state = check_state(board_copy)
+      multiplier = ((board_total) - (board_copy.count("X") + board_copy.count("O")))
+      if cur_state != "Unfinished":
+        # print("Print 8.6")
+        if cur_state == "X win" or cur_state == "O win":
+          # print(B, "Win!!!!!!!!!!", board_copy, y, multiplier, chances)
+          chances[y] += (100 * multiplier)
+          # print(B, "Win!!!2222222", board_copy, y, multiplier, chances)
+        elif cur_state == "Tie":
+          chances[y] += 1
+          # print("TIE!!!!", board_copy, y, multiplier, chances)
+        elif isinstance
+      else:
+        # print("Print 8.7")
+        for z in range(board_total):
+          loop_num += 1
+          board_copy_copy = list(board_copy)
+          # print("LOOP NUMBER: ", loop_num, y, board_copy_copy[y], A, board_copy_copy)
+          # print("Print 8.75", board_copy_copy)
+          if str(board_copy_copy[z]).isdigit():
+            # print("Print 8.8")
+            board_copy_copy[z] = A
+            cur_stateB = check_state(board_copy_copy)
+            multiplier = ((board_total) - (board_copy_copy.count("X") + board_copy_copy.count("O")))
+            # print("Print 8.9")
+            if cur_stateB != "Unfinished":
+              if cur_stateB == "X win" or cur_stateB == "O win":
+                # print(A, "Win!!!!!!!!!!", board_copy_copy, y, multiplier, chances)
+                chances[y] -= (100 * multiplier)
+                # print(A, "Win!!!!22222", board_copy_copy, y, multiplier, chances)
+              elif cur_stateB == "Tie":
+                chances[y] += 1
+                # print("TIE!!!!", board_copy_copy, y, multiplier, chances)
+            else:
+              chances[y] += sum(check_options(board_copy_copy))
+  # print("Chances: ", chances)
+  return chances
+
+run_game()
+
+
+
+
+
+def check_options2(board):
+  """In Progress"""
   # print("Print 8.1")
   global A
   global B
@@ -256,5 +324,3 @@ def check_options(board):
               chances[y] += sum(check_options(board_copy_copy))
   # print("Chances: ", chances)
   return chances
-
-run_game()
